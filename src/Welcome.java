2cc0fa6c85b1d5eba24ada083858e9004f7ed34a
vantruong6112005 <nguyenvantruongdz91@gmail.com>
2024-08-14 23:38:35 +0700

    init project

---
+++ src/Welcome.java
@@ -0,0 +1,8 @@
+public class Welcome
+{
+    public static void main(String[] args) {
+        System.out.println("Hello Van Truong");
+        System.out.println("Welcome to DHKTPM19A");
+
+    }
+}
